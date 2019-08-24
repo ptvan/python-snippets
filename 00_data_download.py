74134c2b3d8c488bf9de17f012b35f10f31ea4a1
Phu <ptv@ponderosa.local>
2019-08-24 09:39:24 -0700

    added EDA

--- 00_data_download.py
+++ 00_data_download.py
@@ -16,9 +16,4 @@ def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
     housing_tgz.extractall(path=housing_path)
     housing_tgz.close()
 
-def load_housing_data(housing_path=HOUSING_PATH):
-    csv_path = os.path.join(housing_path, "housing.csv")
-    return pd.read_csv(csv_path)
-
 fetch_housing_data()
-housing = load_housing_data()
