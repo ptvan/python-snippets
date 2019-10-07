e5f7ce419bd1385f66282244a77938f6682c845e
ptvan <pvan@fhcrc.org>
2019-10-07 15:57:55 -0700

    added MNIST example

--- mnist.py
+++ mnist.py
@@ -11,13 +11,13 @@ train_images = train_images.astype('float32') / 255
 test_images = test_images.reshape((10000, 28 * 28))
 test_images = test_images.astype('float32') / 255
 
-train_labels = to_categorical(train_labels)
-test_labels = to_categorical(test_labels)
-
 network = models.Sequential()
-network.add(layers.Dense(512, activation='relu', input_shape=(28,28)))
+network.add(layers.Dense(512, activation='relu', input_shape=(28*28,)))
 network.add(layers.Dense(10, activation='softmax'))
 network.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+
+train_labels = to_categorical(train_labels)
+test_labels = to_categorical(test_labels)
 
 network.fit(train_images, train_labels, epochs=5, batch_size=128)
 
