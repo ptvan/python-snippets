24b8dbdcd3b030029a5ce5a280bf787057fd07ed
ptvan <pvan@fhcrc.org>
2019-10-07 15:45:41 -0700

    added MNIST example

--- mnist.py
+++ mnist.py
@@ -5,10 +5,10 @@ from keras.utils import to_categorical
 
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
-train_images = train_images.reshapre((60000, 28 * 28))
+train_images = train_images.reshape((60000, 28 * 28))
 train_images = train_images.astype('float32') / 255
 
-test_images = test_images.reshapre((10000, 28 * 28))
+test_images = test_images.reshape((10000, 28 * 28))
 test_images = test_images.astype('float32') / 255
 
 train_labels = to_categorical(train_labels)
