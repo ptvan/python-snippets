be04031f9d0a89c62d58eeeb8d89f9d4f932e287
Phu T. Van <pvan@fredhutch.org>
2020-01-16 09:32:32 -0800

    updated python_notes.py

--- python_notes.py
+++ python_notes.py
@@ -1,22 +1,22 @@
-# iterate over two lists simultaneously
+# LOOPS
 xs = [1, 3, 5, 7]
 ys = [2, 4, 6, 8]
 for x, y in zip(xs, ys):
     print(x, y)
 
-# strings
+# STRINGS
 import re
 mystring = "-my input string, which has 1,2,3 ,4 & lots of other stuff!  "
 mystring.lstrip("-")
 mystring.replace("1", "one")
 re.findall(",", mystring)
 
-# numbers
+# NUMBERS
 import decimal
 decimal.Decimal('2.5') + decimal.Decimal('2.6')
 round(47584, -1)
 
-# file I/O
+# FILE I/O
 import os
 dirnames = [name for name in os.listdir('/Users/ptv/') if os.path.isdir(os.path.join('/Users/ptv', name))]
 
@@ -25,5 +25,6 @@ with gzip.open("myfile.gz", 'rt') as f:
     text = f.read()
 with gzip.open("otherfile.gz", 'wt', compresslevel=5) as f:
     f.write(text)
+# for binary mode and check existing file
 with gzip.open("filethatalreadyexists.gz", 'xb', compresslevel=5) as f:
     f.write(text)
