ea3b5f221a4ba023f54338d8e70db52d9dc5d5db
Phu T. Van <pvan@fredhutch.org>
2020-01-22 08:05:51 -0800

    reorg Keras, Spark and pymc3 code

--- pymc3.py
+++ pymc3.py
@@ -2,6 +2,11 @@ import numpy as np
 import pandas as pd
 import pymc3 as pm
 from matplotlib import pyplot as plt
+import theano.tensor as tt
+
+#####################
+# MODELING STEP COUNTS
+#####################
 
 # load step count data which we already cleaned
 count_data = pd.read_csv("datasets/apple_health_export/stepsData.csv", sep=",")
@@ -64,3 +69,38 @@ data = np.r_[pm.Poisson.dist(lambda_1, tau).random(), pm.Poisson.dist(lambda_2, 
 
 plt.bar(np.arange(2000), data, color="#348ABD")
 plt.bar(tau - 1, data[tau - 1], color="r", label="user behaviour changed")
+
+#####################
+# DETECTING CHEATERS
+#####################
+
+# generate the true cheaters by sampling from Bernoulli
+# note that with pymc 3 the convention is to sample inside a model
+# (though we can use pm.Uniform.dist.(params,).random() as well...)
+
+with pm.Model() as model:
+    N = 100
+    p = pm.Uniform("freq_cheating", 0, 1)
+    true_answers = pm.Bernoulli("truths", p, shape=N, testval=np.random.binomial(1,0.5, N))
+
+# the privacy function, where the student flips coins to determine if they are cheaters
+with model:
+    first_coin_flips = pm.Bernoulli("first_flips", 0.5, shape=N, testval=np.random.binomial(1, 0.5, N))
+    second_coin_flips = pm.Bernoulli("second_flips", 0.5, shape=N, testval=np.random.binomial(1, 0.5, N))
+    val = first_coin_flips * true_answers + (1 - first_coin_flips) * second_coin_flips
+    observed_proportion = pm.Deterministic("observed_proportion", tt.sum(val)/float(N))
+
+# assume we have 35 "Yes" responses
+X = 35
+with model:
+    observations = pm.Binomial("obs", N, observed_proportion, observed=X)
+
+# run the M-H sampling, keeping the traces after the burn-in period...
+with model:
+    step = pm.Metropolis(vars=[p])
+    trace = pm.sample(40000, step=step)
+    burned_trace = trace[15000:]
+
+p_trace = burned_trace["freq_cheating"][15000:]
+plt.hist(p_trace, histtype="stepfilled", normed=True, alpha=0.85, bins=30,
+         label="posterior distribution", color="#348ABD")
