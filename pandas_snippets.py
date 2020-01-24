09cc3ad72328e67ad9da0a387888384b9a99573d
Phu T. Van <pvan@fredhutch.org>
2020-01-24 08:51:06 -0800

    updated pandas_snippets.py

--- pandas_snippets.py
+++ pandas_snippets.py
@@ -2,14 +2,19 @@ import pandas as pd
 
 steps = pd.read_csv("~/working/datasets/iphone_health/stepsData.csv")
 list(steps)
+steps.shape
+steps.head()
 steps = steps.drop(['startDate', 'endDate'], axis=1)
 steps['creationDate'] = steps['creationDate'].str.slice(0, 10)
 steps = steps.groupby('creationDate', as_index=False).sum()
 
 cycling = pd.read_csv("~/working/datasets/iphone_health/cyclingData.csv")
 list(cycling)
+cycling.shape
+cycling.head()
 cycling = cycling.drop(['startDate', 'endDate'], axis=1)
 cycling['creationDate'] = cycling['creationDate'].str.slice(0, 10)
 cycling = cycling.groupby('creationDate', as_index=False).sum()
 
+activity = steps.merge(cycling, 'outer', left_on='creationDate', right_on='creationDate').fillna(0)
 
