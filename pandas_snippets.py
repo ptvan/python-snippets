d37cfbbe6c2b5fce5cf8811d438bb4ca693f7308
Phu T. Van <pvan@fredhutch.org>
2020-01-24 08:44:45 -0800

    reorg Keras, Spark and pymc3 code

--- pandas_snippets.py
+++ pandas_snippets.py
@@ -2,3 +2,14 @@ import pandas as pd
 
 steps = pd.read_csv("~/working/datasets/iphone_health/stepsData.csv")
 list(steps)
+steps = steps.drop(['startDate', 'endDate'], axis=1)
+steps['creationDate'] = steps['creationDate'].str.slice(0, 10)
+steps = steps.groupby('creationDate', as_index=False).sum()
+
+cycling = pd.read_csv("~/working/datasets/iphone_health/cyclingData.csv")
+list(cycling)
+cycling = cycling.drop(['startDate', 'endDate'], axis=1)
+cycling['creationDate'] = cycling['creationDate'].str.slice(0, 10)
+cycling = cycling.groupby('creationDate', as_index=False).sum()
+
+
