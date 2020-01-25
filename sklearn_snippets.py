4125c58d5bfc32d7f1c8821b61f5db5b7ff5cf84
ptvan <pvan@fredhutch.org>
2020-01-25 09:50:38 -0800

    added 1D KDE to sklearn_snippets.py

--- sklearn_snippets.py
+++ sklearn_snippets.py
@@ -1,14 +1,17 @@
 from sklearn.neighbors import KernelDensity
-import pandas as pd 
+import pandas as pd
 import numpy as np
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 steps = pd.read_csv("~/working/datasets/iphone_health/stepsData.csv")
 steps = steps.drop(['startDate','endDate'], axis=1)
 steps['creationDate'] = steps['creationDate'].str.slice(0,10)
 steps = steps.groupby('creationDate', as_index=False).sum()
 
-# Kernel Density Estimation
-stepsraw = steps['stepsWalked'].to_numpy().reshape(1,-1)
+# Kernel Density Estimation 1D Gaussian
+stepsraw = steps['stepsWalked'].to_numpy().reshape(-1,1)
+X_plot = np.linspace(-5, 10,1000)[:, np.newaxis]
 kde = KernelDensity(kernel='gaussian', bandwidth=0.5).fit(stepsraw)
-kde.score_samples(stepsraw)
+log_dens = kde.score_samples(X_plot)
+sns.distplot(np.exp(log_dens))
