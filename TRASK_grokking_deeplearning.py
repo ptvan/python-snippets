8ce624565274f862a59161d5e141c1ce6ac6eaea
Phu T. Van <pvan@fredhutch.org>
2020-01-27 14:12:14 -0800

    fixed typo

--- TRASK_grokking_deeplearning.py
+++ TRASK_grokking_deeplearning.py
@@ -53,7 +53,7 @@ from keras.datasets import mnist
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-images, labels = (x_train[0:1000].reshape(1000, 28*28)  , y_train[0:1000])
+images, labels = (x_train[0:1000].reshape(1000, 28*28) / 255  , y_train[0:1000])
 one_hot_labels = np.zeros((len(labels), 10))
 
 for i,l in enumerate(labels):
