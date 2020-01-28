f13512f5efcd5310a2bc42c8195f3d57904b7880
Phu T. Van <pvan@fredhutch.org>
2020-01-28 08:05:42 -0800

    updated TRASK_grokking_deeplearning.py

--- TRASK_grokking_deeplearning.py
+++ TRASK_grokking_deeplearning.py
@@ -45,8 +45,9 @@ for j in range(60000):
 print("Output After Training:")
 print(l1)
 
-
-## M.N.I.S.T. 
+##################################
+# M.N.I.S.T. using a basic network
+################################## 
 import sys
 import numpy as np
 from keras.datasets import mnist
@@ -68,7 +69,7 @@ for i,l in enumerate(y_test):
 
 np.random.seed(1)
 relu = lambda x:(x>0) * x
-relu2derive = lambda x: x>=0
+relu2deriv = lambda x: x>=0
 alpha = 0.005
 iterations = 350
 hidden_size = 40
@@ -87,7 +88,7 @@ for j in range(iterations):
         error += np.sum((labels[i:i+1] - layer_2) ** 2)
         correct_cnt += int(np.argmax(layer_2) == np.argmax(labels[i:i+1]))
         layer_2_delta = (labels[i:i+1] - layer_2)
-        layer_1_delta = layer_2_delta.dot(weights_1_2.T) * relu2derive(layer_1)
+        layer_1_delta = layer_2_delta.dot(weights_1_2.T) * relu2deriv(layer_1)
         weights_1_2 += alpha * layer_1.T.dot(layer_2_delta)
         weights_0_1 += alpha * layer_0.T.dot(layer_1_delta)
 
@@ -95,3 +96,42 @@ for j in range(iterations):
                     " I:"+str(j)+ \
                     " Error:" + str(error/float(len(images)))[0:5] + \
                     " Correct:" + str(correct_cnt/float(len(images))))
+
+#########################
+# M.N.I.S.T. with dropout
+#########################
+# variables same as above
+
+for j in range(iterations):
+    error, correct_cnt = (0.0, 0)
+    for i in range(len(images)):
+        layer_0 = images[i:i+1]
+        layer_1 = relu(np.dot(layer_0, weights_0_1))
+        dropout_mask = np.random.randint(2, size=layer_1.shape)
+        layer_1 *= dropout_mask * 2
+        layer_2 = np.dot(layer_1, weights_1_2)
+
+        error += np.sum((labels[i:i+1] - layer_2) ** 2)
+        correct_cnt += int(np.argmax(layer_2) == np.argmax(labels[i:i+1]))
+        layer_2_delta = layer_2_delta.dot(weights_1_2.T) * relu2deriv(layer1)
+        layer_1_delta *= dropout_mask
+
+        weights_1_2 += alpha * layer_1.T.dot(layer_2_delta)
+        weights_0_1 += alpha * layer_0.T.dot(layer_1_delta)
+    
+    if (j%10 == 0):
+        test_error = 0.0
+        test_correct_cnt = 0
+        for i in range(len(test_images)):
+            layer_0 = test_images[i:i+1]
+            layer_1 = relu(np.dot(layer_0, weights_0_1))
+            layer_2 = np.dot(layer_1, weights_1_2)
+            test_error += np.sum((test_labels[i:i+1] - layer_2) ** 2)
+            test_correct_cnt += int(np.argmax(layer_2) == np.argmax(test_labels[i:i+1]))
+        sys.stdout.write("\n"+ \
+                        "I:" + str(j) + \
+                         " Test-Err:" + str(test_error / float(len(test_images)))[0:5] + \
+                         " Test-Acc:" + str(test_correct_cnt / float(len(test_images)))+\
+                         " Train-Err:" + str(error / float(len(images)))[0:5] + \
+                         " Train-Acc:" + str(correct_cnt / float(len(images))))
+
