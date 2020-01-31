e849b5780809e636a73bdfd6ea37c48a0a7c7a4c
Phu T. Van <pvan@fredhutch.org>
2020-01-31 08:27:17 -0800

    updated sklearn_snippets.py

--- sklearn_snippets.py
+++ sklearn_snippets.py
@@ -1,8 +1,10 @@
-from sklearn.neighbors import KernelDensity
+import sklearn.datasets
 import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.neighbors import KernelDensity
+from sklearn.ensemble import RandomForestRegressor
 
 steps = pd.read_csv("~/working/datasets/iphone_health/stepsData.csv")
 steps = steps.drop(['startDate','endDate'], axis=1)
@@ -15,3 +17,8 @@ X_plot = np.linspace(-5, 10,1000)[:, np.newaxis]
 kde = KernelDensity(kernel='gaussian', bandwidth=0.5).fit(stepsraw)
 log_dens = kde.score_samples(X_plot)
 sns.distplot(np.exp(log_dens))
+
+# Random Forest
+raw = sklearn.datasets.load_boston()
+boston = pd.DataFrame(raw.data)
+boston.columns = raw.feature_names
