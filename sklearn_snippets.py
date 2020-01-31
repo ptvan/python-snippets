b646a6c0af65c60c243f0423ba57624b4b81f125
Phu T. Van <pvan@fredhutch.org>
2020-01-31 08:37:21 -0800

    updated sklearn_snippets.py

--- sklearn_snippets.py
+++ sklearn_snippets.py
@@ -5,6 +5,8 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.neighbors import KernelDensity
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
 
 steps = pd.read_csv("~/working/datasets/iphone_health/stepsData.csv")
 steps = steps.drop(['startDate','endDate'], axis=1)
@@ -19,6 +21,12 @@ log_dens = kde.score_samples(X_plot)
 sns.distplot(np.exp(log_dens))
 
 # Random Forest
-raw = sklearn.datasets.load_boston()
-boston = pd.DataFrame(raw.data)
-boston.columns = raw.feature_names
+X, y = sklearn.datasets.load_boston(return_X_y=True)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+regressor = RandomForestRegressor(n_estimators=20, random_state=0)
+regressor.fit(X_train, y_train)
+y_pred = regressor.predict(X_test)
+print('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
+print('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+
