3518a7219f18be8c47ed626eb970f8b2adbcae7d
ptvan <pvan@fredhutch.org>
2020-03-10 06:18:51 -0700

    added autoencoder

--- keras_snippets.py
+++ keras_snippets.py
@@ -2,6 +2,9 @@ from keras import layers
 from keras import models
 from keras.datasets import mnist
 from keras.utils import to_categorical
+from models.AE import Autoencoder
+
+## MNIST
 
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
@@ -23,3 +26,15 @@ network.fit(train_images, train_labels, epochs=5, batch_size=128)
 
 test_loss, test_acc = network.evaluate(test_images, test_labels)
 print('test_acc:', test_acc)
+
+## variational autoencoders
+
+AE = Autoencoder(
+    input_dim = (28,28,1)
+    , encoder_conv_filters = [32,64,64, 64]
+    , encoder_conv_kernel_size = [3,3,3,3]
+    , encoder_conv_strides = [1,2,2,1]
+    , decoder_conv_t_filters = [64,64,32,1]
+    , decoder_conv_t_kernel_size = [3,3,3,3]
+    , decoder_conv_t_strides = [1,2,2,1]
+    , z_dim = 2)
