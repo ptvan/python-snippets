1be1bba996455585381bda3c3849eb30b56fab45
ptvan <pvan@fredhutch.org>
2020-04-15 08:24:00 -0700

    added multidimensional scaling

--- dimensionreduction_snippets.py
+++ dimensionreduction_snippets.py
@@ -3,9 +3,12 @@ import numpy as np
 import sklearn.datasets
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
+from sklearn.manifold import MDS
 
 # PCA
-
 dat, x = sklearn.datasets.make_swiss_roll(n_samples=10000, noise=0.05, random_state=500)
 princomp = PCA(n_components=2).fit(dat)
 print(princomp.explained_variance_ratio_)
+
+# MDS
+multidim = MDS(n_components=2).fit_transform(dat)
