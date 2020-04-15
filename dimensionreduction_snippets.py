ddd0d7528a5aa5b08204c8aaf0f4f9702d02b26b
ptvan <pvan@fredhutch.org>
2020-04-15 08:13:51 -0700

    added PCA

--- dimensionreduction_snippets.py
+++ dimensionreduction_snippets.py
@@ -1,2 +1,11 @@
 import pandas as pd
 import numpy as np
+import sklearn.datasets
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+
+# PCA
+
+dat, x = sklearn.datasets.make_swiss_roll(n_samples=10000, noise=0.05, random_state=500)
+princomp = PCA(n_components=2).fit(dat)
+print(princomp.explained_variance_ratio_)
