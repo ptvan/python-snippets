82344c634cef9b10624d99819da947779a577f82
ptvan <pvan@fredhutch.org>
2020-05-05 06:50:22 -0700

    updated layout

--- pyQt_filerenamer.py
+++ pyQt_filerenamer.py
@@ -5,36 +5,41 @@ from PyQt5.QtWidgets import (QApplication, QCheckBox, QComboBox, QDateTimeEdit,
         QDial, QDialog, QGridLayout, QGroupBox, QHBoxLayout, QLabel, QLineEdit,
         QProgressBar, QPushButton, QRadioButton, QScrollBar, QSizePolicy,
         QSlider, QSpinBox, QStyleFactory, QTableWidget, QTableWidgetItem, QTabWidget, QTextEdit,
-        QVBoxLayout, QWidget, QFileDialog)
-
+        QVBoxLayout, QWidget, QFileDialog, QMessageBox)
 
 class filerenamer(QWidget):
     def __init__(self, parent = None):
         super(filerenamer, self).__init__(parent)
 		
-        layout = QVBoxLayout()
-        self.inputLbl = QLabel("Input files")
-        self.browseBtn = QPushButton("select")
+        layout = QGridLayout()
+        self.setLayout(layout)
+
+        self.browseBtn = QPushButton("select files")
         self.browseBtn.clicked.connect(self.getfiles)
 
         self.prefixChkbox = QCheckBox("prefix")
+        self.prefixEdit = QLineEdit("")
+        self.prefixEdit.setFixedWidth(50)
         self.suffixChkbox = QCheckBox("suffix")
+        self.suffixEdit = QLineEdit("")
+        self.suffixEdit.setFixedWidth(50)
         self.replaceChkbox = QCheckBox("replace")
-        self.replaceEditold = QLineEdit("oldtext")
-        self.replaceEditold.setFixedWidth(100)
-        self.replaceEditnew = QLineEdit("newtext")
-        self.replaceEditnew.setFixedWidth(100)
-
-        layout.addWidget(self.inputLbl)
-        layout.addWidget(self.browseBtn)
-        layout.addWidget(self.prefixChkbox)
-        layout.addWidget(self.suffixChkbox) 
-        layout.addWidget(self.replaceChkbox)
-        layout.addWidget(self.replaceEditold)
-        layout.addWidget(self.replaceEditnew) 
+        self.replaceoldEdit = QLineEdit("oldtext")
+        self.replaceoldEdit.setFixedWidth(190)
+        self.replacenewEdit = QLineEdit("newtext")
+        self.replacenewEdit.setFixedWidth(190)
+        
+        layout.addWidget(self.browseBtn, 0, 0, 1, 3)
+        layout.addWidget(self.prefixChkbox, 1, 0, 1, 1)
+        layout.addWidget(self.prefixEdit, 1, 1, 1, 1)
+        layout.addWidget(self.suffixChkbox, 2, 0, 1, 1) 
+        layout.addWidget(self.suffixEdit, 2, 1, 1, 1)
+        layout.addWidget(self.replaceChkbox, 3, 0, 1, 1)
+        layout.addWidget(self.replaceoldEdit, 3, 1, 1, 1)
+        layout.addWidget(self.replacenewEdit, 3, 2, 1, 1) 
 
         self.contents = QTableWidget()
-        layout.addWidget(self.contents)
+        layout.addWidget(self.contents,4,0,1,3)
         self.setLayout(layout)
         self.setWindowTitle("File Renamer")
         self.contents.setFixedWidth(600)
@@ -45,7 +50,7 @@ class filerenamer(QWidget):
         self.contents.setColumnWidth(1,299)
 
         self.renameBtn = QPushButton("rename")
-        self.renameBtn.clicked.connect(self.renamefiles)
+        self.renameBtn.clicked.connect(self.confirm)
 
         layout.addWidget(self.renameBtn)
 
@@ -59,7 +64,15 @@ class filerenamer(QWidget):
                 item = QTableWidgetItem(ntpath.basename(files[i]))
                 self.contents.setItem(i,0, item)
             print(files)
-    
+
+    def confirm(self):
+        reply = QMessageBox()
+        reply.setWindowTitle("Confirm file rename")
+        reply.setText("Rename files?")
+        reply.setStandardButtons(QMessageBox.Yes|QMessageBox.No)
+        retval = reply.exec_()
+        print("value of pressed message box button:", retval)
+
     def processnames(self):
         return(files)
 
