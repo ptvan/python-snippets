27722cf30df9a21972b95c17ce53be8da07c5622
ptvan <pvan@fredhutch.org>
2020-06-11 16:48:33 -0700

    added feature agglomeration

--- dimensionreduction_snippets.py
+++ dimensionreduction_snippets.py
@@ -4,6 +4,7 @@ import sklearn.datasets
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.manifold import MDS
+from sklearn import cluster
 
 # PCA
 dat, x = sklearn.datasets.make_swiss_roll(n_samples=10000, noise=0.05, random_state=500)
@@ -12,3 +13,10 @@ print(princomp.explained_variance_ratio_)
 
 # MDS
 multidim = MDS(n_components=2).fit_transform(dat)
+
+# Feature agglomeration
+agglo = cluster.FeatureAgglomeration(connectivity=connectivity,
+                                     n_clusters=32)
+
+agglo.fit(dat)
+dat_reduced = agglo.transform(dat)
