fe0151320e608e3a51b69527f505a03407a7b027
ptvan <pvan@fredhutch.org>
2020-06-12 07:04:21 -0700

    updates

--- hypothesistesting_snippets.py
+++ hypothesistesting_snippets.py
@@ -1,7 +1,12 @@
 from scipy.stats import normaltest
 import random
 
-# D'Agostino's test for normality
 dat = [random.randrange(1,50,1) for i in range(50)]
+
+# D'Agostino's test for normality
 stat, p = normaltest(dat)
 print('stat=%.3f, p=%.3f' % (stat, p)) 
+
+# Anderson-Darling test for normality
+result = anderson(data)
+print('stat=%.3f' % (result.statistic))
