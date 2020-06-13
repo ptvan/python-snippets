c5130ecf2b59cc54057f976bd25bae3099746ea4
ptvan <pvan@fredhutch.org>
2020-06-13 06:38:58 -0700

    updates

--- hypothesistesting_snippets.py
+++ hypothesistesting_snippets.py
@@ -1,4 +1,4 @@
-from scipy.stats import normaltest, anderson
+from scipy.stats import normaltest, anderson, chi2_contigency
 import random
 
 dat = [random.randrange(1,50,1) for i in range(50)]
@@ -10,3 +10,7 @@ print('stat=%.3f, p=%.3f' % (stat, p))
 # Anderson-Darling test for normality
 result = anderson(dat)
 print('stat=%.3f' % (result.statistic))
+
+# Chi-Squared test
+table = [[10, 20, 30],[6,  9,  17]]
+stat, p, dof, expected = chi2_contingency(table)
