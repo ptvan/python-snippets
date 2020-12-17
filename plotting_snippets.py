df364279127fffc50c41147f0d11e9c08749d96d
ptvan <pvan@fredhutch.org>
2020-12-17 06:26:08 -0800

    added matplotlib venn

--- plotting_snippets.py
+++ plotting_snippets.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import seaborn as sns
 import numpy as np
+from matplotlib_venn import venn2, venn2_circles, venn2_unweighted
+from matplotlib_venn import venn3, venn3_circles
+from matplotlib import pyplot as plt
 
 steps = pd.read_csv("~/working/datasets/iphone_health/stepsData.csv")
 list(steps)
@@ -20,10 +23,19 @@ cycling = cycling.groupby('creationDate', as_index=False).sum()
 
 activity = steps.merge(cycling, 'outer', left_on='creationDate', right_on='creationDate').fillna(0)
 activity.rename(columns={"creationDate" : "date"})
-
 activity.head()
-sns.distplot(activity['stepsWalked'])
 
-## Seaborn
+## Matplotlib
+
+# venn diagrams
+venn2(subsets = (30, 10, 5), set_labels = ('Group A', 'Group B'))
+
+
+
+## Seaborn 
+
+# scatterplot
 sns.scatterplot(x="stepsWalked", y="milesCycled", data=activity)
 
+# histogram
+sns.distplot(activity['stepsWalked'])
