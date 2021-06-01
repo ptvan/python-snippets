0d295ebe7749140ab54ac6bc23acce3cd284cd16
ptvan <nulzilla@gmail.com>
2021-06-01 05:46:56 -0700

    updates

--- pysam_snippets.py
+++ pysam_snippets.py
@@ -1,7 +1,10 @@
 from pysam import VariantFile
 
+# reading/writing BAMs
+samfile = pysam.AlignmentFile("mybam.bam", "rb")
+
 # reading/writing VCFs
-bcf_in = VariantFile("myfile.bcf")  
+bcf_in = VariantFile("mybcf.bcf")  
 bcf_out = VariantFile('-', 'w', header=bcf_in.header)
 
 for rec in bcf_in.fetch('chr1', 100000, 200000):
