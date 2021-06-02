a70a1c43af24629b1c13931bea68223159ade50a
Phu T. Van <nulzilla@gmail.com>
2021-06-02 07:12:37 -0700

    updates

--- pysam_snippets.py
+++ pysam_snippets.py
@@ -1,7 +1,7 @@
-from pysam import VariantFile
+import pysam
 
 # reading/writing BAMs
-samfile = pysam.AlignmentFile("mybam.bam", "rb")
+samfile = pysam.AlignmentFile("myfile.bam", "rb")
 
 # reading/writing VCFs
 bcf_in = VariantFile("mybcf.bcf")  
@@ -9,3 +9,8 @@ bcf_out = VariantFile('-', 'w', header=bcf_in.header)
 
 for rec in bcf_in.fetch('chr1', 100000, 200000):
     bcf_out.write(rec)
+
+bcf_out.close()
+
+# equivalent to `samtools sort -o output.bam myfile.bam`
+pysam.sort("-o", "output.bam", "myfile.bam")
